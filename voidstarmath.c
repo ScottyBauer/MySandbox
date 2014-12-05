2746762659b8ee2cfb5e890c045bc61d19de06b1
Scott Bauer <sbauer@eng.utah.edu>
2014-12-05 11:52:16 -0700

    checking what the hell void* math does
    Signed-off-by: Scott Bauer <sbauer@eng.utah.edu>

---
+++ voidstarmath.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+/* compiled with -m32 */
+
+
+int main(void)
+{
+
+	unsigned int location;
+	void *vlocation = &location;
+	char *clocation = (char *) &location;
+	
+	assert(sizeof(void *) == 4);
+	assert(sizeof(char*) == 4);
+	
+	assert(vlocation == clocation);
+	assert((vlocation+1) == (clocation+4));
+	assert((vlocation+4) == (clocation+16));
+	return EXIT_SUCCESS;
+}
